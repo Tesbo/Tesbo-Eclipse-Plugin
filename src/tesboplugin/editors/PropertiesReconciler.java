89536f91d56577dc3752c293ad6d5c3f032f894c
nishil <nishilpatel81@gmail.com>
2019-10-21 21:30:57 +0530

    Add color code

--- src/tesboplugin/editors/PropertiesReconciler.java
+++ src/tesboplugin/editors/PropertiesReconciler.java
@@ -21,21 +21,29 @@ public class PropertiesReconciler extends PresentationReconciler {
 
     public PropertiesReconciler() {
     	
-      	Color redColor = Display.getCurrent().getSystemColor(SWT.COLOR_DARK_RED);
 		 Color commentColor = Display.getCurrent().getSystemColor(SWT.COLOR_DARK_GREEN);
 	        RuleBasedScanner scanner = new RuleBasedScanner();
 	        
-	        SingleLineRule quote = new SingleLineRule("'", "'", new Token(new TextAttribute(new Color(Display.getCurrent(),234,177, 63))));
+	        SingleLineRule quote = new SingleLineRule("'", "'", new Token(new TextAttribute(new Color(Display.getCurrent(),153, 51, 255))));
 	        SingleLineRule quote2 = new SingleLineRule("\"", "\"", new Token(new TextAttribute(new Color(Display.getCurrent(),234,177,63))));
+	        
+	        SingleLineRule tags = new SingleLineRule("#", " ", new Token(new TextAttribute(new Color(Display.getCurrent(),0, 204, 0))));
 	       
 	        TesboKeywordRule wordRule = new TesboKeywordRule();
 	        
 	        PatternRule patternRule = new PatternRule("//", null, new Token(new TextAttribute(commentColor)), (char)0, true);
 	       
-	        PatternRule locatorRule = new PatternRule("@", " ", new Token(new TextAttribute(new Color(Display.getCurrent(), 215,79,102))), (char)0, true);
-
-	        PatternRule TestRule = new PatternRule("Test:", null, new Token(new TextAttribute(new Color(Display.getCurrent(), 243,67,62))), (char)0, true);
-	        scanner.setRules(new IRule[] { quote, quote2, wordRule, patternRule,locatorRule,TestRule});
+	        PatternRule locatorRule = new PatternRule("@", " ", new Token(new TextAttribute(new Color(Display.getCurrent(), 102, 102, 0))), (char)0, true);
+	        
+	        PatternRule code = new PatternRule("Code:", null, new Token(new TextAttribute(new Color(Display.getCurrent(), 102, 140, 255))), (char)0, true);
+	        PatternRule TestRule = new PatternRule("Test:", null, new Token(new TextAttribute(new Color(Display.getCurrent(), 0, 0, 255))), (char)0, true);
+	        	   
+	        PatternRule curlyBraces = new PatternRule("{", "}", new Token(new TextAttribute(new Color(Display.getCurrent(), 153, 51, 255))), (char)0, true);
+     	   
+	        PatternRule index = new PatternRule("<", ">", new Token(new TextAttribute(new Color(Display.getCurrent(), 153, 51, 255))), (char)0, true);
+	        PatternRule session = new PatternRule("[", "]", new Token(new TextAttribute(new Color(Display.getCurrent(), 0, 0, 255))), (char)0, true);
+	        
+	        scanner.setRules(new IRule[] { quote, quote2, wordRule, patternRule,locatorRule,TestRule,tags,code,curlyBraces,index,session});
 	        DefaultDamagerRepairer dr = new DefaultDamagerRepairer(scanner);
 	        this.setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
 	        this.setRepairer(dr, IDocument.DEFAULT_CONTENT_TYPE);
--- src/tesboplugin/editors/TesboKeywordRule.java
+++ src/tesboplugin/editors/TesboKeywordRule.java
@@ -2,50 +2,59 @@ package tesboplugin.editors;
 
 import org.eclipse.jface.text.TextAttribute;
 import org.eclipse.jface.text.rules.Token;
-import org.eclipse.jface.text.rules.WordPatternRule;
 import org.eclipse.jface.text.rules.WordRule;
-import org.eclipse.jface.text.templates.GlobalTemplateVariables.WordSelection;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.RGB;
+
 import org.eclipse.swt.widgets.Display;
-
-
 
 public class TesboKeywordRule extends WordRule {
 
 	public TesboKeywordRule() {
 		super(new TesboDetector());
-		
-		String  tesboKeyword[] = {"click","pause","right","Click","Enter","Pause"};
-		
-		
-		Color VerifyColor= new Color(Display.getCurrent(),202,108, 42);
-		Color TestColor= new Color(Display.getCurrent(),169, 234, 58);
-		Color CodeColor= new Color(Display.getCurrent(),31, 145, 193);
-		Color StepColor= new Color(Display.getCurrent(),70, 132, 43);
-		Color tesboKeysColor = new Color(Display.getCurrent(),62,101,208);
-		
-		
+
+		String tesboKeyword[] = { "click", "Click", "from", "From","list","List", "offset", "Offset", "and", "And", "hold",
+				"Hold", "click", "Click", "scroll", "Scroll", "right", "Right", "click", "Click", "double", "Double",
+				"print", "Print", "capture", "Capture", "Screenshot", "screenshot", "of", "clear", "Clear", "cookies",
+				"Cookies", "cache", "Cache", "press", "Press", "Enter", "enter", "tab", "Tab", "plus", "Plus", "ctrl",
+				"Ctrl", "clear", "Clear", "upload", "Upload", "file", "File", "Get", "get", "Page", "page", "source",
+				"Source", "Open", "open", "url", "URL", "Url", "switch", "Switch", "active", "Active", "element",
+				"Element", "alert", "Alert", "accept", "Accept", "close", "Close", "cancel", "Cancel", "verify",
+				"Verify", "text", "Text", "default", "Default", "content", "Content", "frame", "Frame", "using",
+				"Using", "id", "Id", "name", "Name", "parent", "Parent", "main", "Main", "new", "New", "window",
+				"Window", "navigate", "Navigate", "back", "Back", "forward", "Forward", "Refresh", "refresh", "bottom",
+				"Bottom", "top", "Top", "horizontal", "Horizontal", "text", "Text", "index", "Index", "value", "Value",
+				"Resize", "resize", "Minimize", "minimize", "Maximize", "maximize", "Mouse", "Mouse", "Hover", "hover",
+				"size", "size", "Not", "not", "equal", "Equal", "ignore", "Ignore", "case", "Case", "contains",
+				"Contains", "start", "Start", "With", "with", "end", "number", "Number", "alphanumeric",
+				"Alphanumeric", "displayed", "Displayed", "present", "Present", "visible", "Visible", "title", "Title",
+				"check", "Check", "available", "Available", "Current", "current", "pause", "Pause", "disappear",
+				"Disappear", "clickable", "Clickable", "display", "Display", "sec", "Sec", "SEC", "coordinate",
+				"Coordinate","last","Last","First","first","grater","Grater","then","Then","Deselect","deselect","Select","select"};
+
+		Color VerifyColor = new Color(Display.getCurrent(), 0, 204, 0);
+		Color tesboKeysColor = new Color(Display.getCurrent(), 179, 89, 0);
+		Color StepColor = new Color(Display.getCurrent(), 153, 51, 255);
+		Color dataSet = new Color(Display.getCurrent(), 71, 209, 209);
+		Color ifCondition = new Color(Display.getCurrent(), 102, 0, 102);
+
 		addWord("Step:", new Token(new TextAttribute(StepColor)));
-		addWord("Code:", new Token(new TextAttribute(CodeColor)));
 		addWord("Verify:", new Token(new TextAttribute(VerifyColor)));
-		addWord("End", new Token(new TextAttribute(new Color(Display.getCurrent(), 243,67,62))));
-	//	addWord("Test:", new Token(new TextAttribute(TestColor)));
+		addWord("End", new Token(new TextAttribute(new Color(Display.getCurrent(), 0, 0, 255))));
+		addWord("DataSet:", new Token(new TextAttribute(dataSet)));
+		addWord("If::", new Token(new TextAttribute(ifCondition)));
+		addWord("Else", new Token(new TextAttribute(ifCondition)));
+		addWord("Else::", new Token(new TextAttribute(ifCondition)));
+		addWord("End::", new Token(new TextAttribute(ifCondition)));
+		addWord("Session:", new Token(new TextAttribute(dataSet)));
 		
 		
 		
-		for(int i = 0;i < tesboKeyword.length;i++)
-		{
-			
-			addWord(tesboKeyword[i], new Token(new TextAttribute(tesboKeysColor)));
-			
-			
+		for(int i = 0;i < tesboKeyword.length;i++) { 
+		  addWord(tesboKeyword[i], new Token(new TextAttribute(tesboKeysColor)));
+		 
 		}
-		
-		
-		
-		
+		 
+
 		// TODO Auto-generated constructor stub
 	}
 
