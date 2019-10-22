c2190ee97af6ae83f42df5ba42d9b36c21077577
Viral Patel <vir@qable.io>
2019-10-22 17:06:34 +0530

    Updated Merge Color Code branch to master

--- src/tesboplugin/editors/TesboContectAssistence.java
+++ src/tesboplugin/editors/TesboContectAssistence.java
@@ -1,5 +1,6 @@
 package tesboplugin.editors;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.eclipse.jface.text.BadLocationException;
@@ -13,64 +14,93 @@ import org.eclipse.jface.text.contentassist.IContextInformationValidator;
 
 public class TesboContectAssistence implements IContentAssistProcessor {
 
-	 public static final String[] PROPOSALS = new String[] { "Test:", "Step:", "Verify:" };
-	
+	public static final String[] PROPOSALS = new String[] {"Test:","Step:","Verify:","End","Click","from list","offset","and hold","Scroll","and click","Right click","Double click",
+			"Print","Capture screenshot","Capture screenshot of","Clear cookies","Clear cache",
+			"Press","Enter","tab","Tab","plus A","plus C","plus V","ctrl","clear","Upload file","Get page source","Open URL",
+			"Switch","active element","alert","accept","close","cancel","Verify text","default content","frame using id","frame using name",
+			"parent frame","main frame","frame","new window","main window","parent window","Navigate","back","forward","refresh page","bottom",
+			"top","horizontal","text","index","value","Window resize","Window minimize","Window maximize",
+			"Mouse hover","Close window","size","Not equal","equal","ignore case","contains","start with","end with",
+			"number","alphanumeric","displayed","present","visible","page title","get cookies","check","is available","current URL",
+			"is equal to","equal to","is contains","Pause","disappear","clickable","display","sec","coordinate","first element","last element",
+			"less then","grater then","Select", "using text","using value","using index"};
+
 	@Override
 	public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int offset) {
 		IDocument document = viewer.getDocument();
 
 		try {
-			
-			
-			int lineOfOffset = document.getLineOfOffset(offset);
-			int lineOffset = document.getLineOffset(lineOfOffset);
+
+
+			int lineNumber = document.getLineOfOffset(offset);
+
+			int lineOffset = document.getLineOffset(lineNumber);
+
+
+
 
 			int lineTextLenght = offset - lineOffset;
+
+
 			String lineStartToOffsetValue = document.get(lineOffset, lineTextLenght).toLowerCase();
 
-			
 
-			
-			return Arrays.asList(PROPOSALS).stream().filter(proposal ->  proposal.toLowerCase().startsWith(lineStartToOffsetValue)).map(proposal -> new CompletionProposal(proposal, lineOffset, lineTextLenght, proposal.length())).toArray(ICompletionProposal[]::new);
-			
-			
-			
-			
+			System.out.println("+++++++++++++++++++++++++++++++");
+			System.out.println("offset" + offset);
+			System.out.println("lineNumber  " + lineNumber);
+			System.out.println("lineOffset  " + lineOffset);
+			System.out.println("lineTextLenght  " + lineTextLenght);
+			System.out.println("lineStartToOffsetValue  " + lineStartToOffsetValue);
 
-			
-			
-			
-		/*	return Arrays.asList(PROPOSALS).stream()
-					.filter(proposal -> !viewer.getDocument().get().contains(proposal)
-							&& proposal.toLowerCase().startsWith(lineStartToOffsetValue))
-					.map(proposal -> new CompletionProposal(proposal, lineOffset, lineTextLenght, proposal.length()))
-					.toArray(ICompletionProposal[]::new);*/
+
+			String [] LineArray =
+					lineStartToOffsetValue.split(" ");
+
+			for (int i = 0; i< LineArray.length;i++)
+			{		System.out.println("LineArray[ "+i+"]"+ LineArray[i]);
+			}
+
+			System.out.println("Start With " + LineArray[LineArray.length-1]);
+			System.out.println("-------------------------------");
+
+
+
+
+
+
+
+
+
+			return Arrays.asList(PROPOSALS).stream()
+					.filter(proposal ->
+							proposal.toLowerCase().startsWith(LineArray[LineArray.length-1]))
+					.map(proposal -> new CompletionProposal(proposal, (offset-LineArray[LineArray.length-1].length()), LineArray[LineArray.length-1].length(), proposal.length()))
+					.toArray(ICompletionProposal[]::new);
+
 		} catch (BadLocationException e) {
 			e.printStackTrace();
 		}
-		
-		System.out.println("Return From HERE ");
 		return new ICompletionProposal[0];
-		
+
 	}
 
 	@Override
 	public IContextInformation[] computeContextInformation(ITextViewer viewer, int offset) {
 		// TODO Auto-generated method stub
-		
-		
-		System.out.println("Viewer" + viewer + "     " + offset);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		return null;
 	}
 
